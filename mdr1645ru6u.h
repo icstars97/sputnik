555d1c41c9f6306b5886d8e2bf1b0191ff3b4d1d
EvgenShSg <zhenia97-97@mail.ru>
2018-11-27 18:24:40 +0300

    v.0.3 Bug fixed. Next: Global Read.

--- mdr1645ru6u.h
+++ mdr1645ru6u.h
@@ -5,13 +5,13 @@
 
 
 //write data bits to ram 
-void ram_write( uint32_t cell_addr, uint16_t *Template);
+void RAM_write( uint32_t cell_addr, uint16_t *Template);
 
 //Set Addres
-void Ram_Addr_Set(uint32_t Cell_Addr);
+void RAM_Addr_Set(uint32_t Cell_Addr);
 
 //read data bits from ram
-void ram_read(uint32_t cell_addr, uint16_t *Read_Array);
+void RAM_read(uint32_t cell_addr, uint16_t *Read_Array);
 
 //Compare Read_Array (ram_data) and Template (Pattern)
-uint64_t compare_data(uint16_t pattern[8], uint16_t ram_data[8],uint32_t cell_addr);
+uint32_t* compare_data(uint16_t pattern[8],uint16_t ram_data[8],uint32_t cell_addr);

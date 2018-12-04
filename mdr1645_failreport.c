506ec861975ec7710a8b634d12163a0bccfa5424
andrei <and15rei@gmail.com>
2018-12-05 00:04:25 +0300

    storage info in global variable now

--- mdr1645_failreport.c
+++ mdr1645_failreport.c
@@ -65,40 +65,39 @@ uint32_t* build_FailArray(uint16_t pattern[8],uint16_t ram_data[8],uint32_t cell
 		}
 			return &fail_arr[0];
 }
-//storage[0] reserved for report
+//storage_info
 //			XXXX 		XXXX 		XXXX 		XXXX 		XXXX 		XXXX 		XXXX 		XX00 
 //		  |space left      | failure count   |last timestamp number|
-uint8_t RAM_SaveFailArray(uint32_t* storage,uint32_t* fail_arr)
+uint8_t RAM_SaveFailArray(uint32_t* storage,uint32_t* fail_arr, uint32_t* storage_info)
 {
 	uint16_t errcount,index,timestamp_pos,i=1;
-	if (((storage[0] & (0x3FF<<22))>>22)<999)
+	if (((*storage_info & (0x3FF<<22))>>22)<999)
 	{
-		index=999-((storage[0] & (0x3FF<<22))>>22)+2;
-		errcount=(storage[0] & (0x3FF<<12))>>12;
-		timestamp_pos=index-1;
-		storage[index-1]=fail_arr[0];
-		while(!(fail_arr[i] & (1<<0)))
+		index=999-((*storage_info  & (0x3FF<<22))>>22)+1; // start position = total volume - free space + 1
+		errcount=(*storage_info & (0x3FF<<12))>>12; // copy total error count into local variable
+		storage[index]=fail_arr[0]; // save timestamp to storage
+		while(!(fail_arr[i] & (1<<0)))  //check if current element of fail array is last one (lsb=1)
 		{
-			storage[index]=fail_arr[i];
+			storage[index]=fail_arr[i]; // save failure word to storage
 			i++;
 			index++;
-			if (fail_arr[i] & (0xF<<1)) errcount+=2;
+			if (fail_arr[i] & (0xF<<1)) errcount+=2; //check if 1 or 2 failure are in word, then then add 1 or 2 to counter variable
 			else errcount++;
 		}
-		storage[0]=((999-index)<<22) | (errcount<<12) | (timestamp_pos<<2);
+		*storage_info=((999-index)<<22) | (errcount<<12) | (timestamp_pos<<2); // rewrite fields of storage_info with new data
 		
 	}
-	return ((storage[0] & (0x3FF<<22))>>22);
+	return ((storage[0] & (0x3FF<<22))>>22);//ya ne znayu zachem eto zdes no udalyat poka ne budu
 	
 }
 
 
-RAM_FailReport GetFailReport(uint32_t* storage)
+RAM_FailReport GetFailReport(uint32_t* storage_info)//receives storage_info and copies its' fields into structure
 {
 	RAM_FailReport report;
-	report.failcount=(storage[0] & (0x3FF<<12))>>12;
-	report.free_space=(storage[0] & (0x3FF<<22))>>22;
-	report.last_timestamp=(storage[0] & (0x3FF<<2))>>2;
+	report.failcount=(*storage_info & (0x3FF<<12))>>12;
+	report.free_space=(*storage_info & (0x3FF<<22))>>22;
+	report.last_timestamp=(*storage_info & (0x3FF<<2))>>2;
 	return report;
 	
 	
--- mdr1645_failreport.h
+++ mdr1645_failreport.h
@@ -21,6 +21,6 @@ typedef struct{
 uint32_t* build_FailArray(uint16_t pattern[8],uint16_t ram_data[8],uint32_t cell_addr); //compare data extracted from RAM with data in template
 
 
-uint8_t RAM_SaveFailArray(uint32_t* storage,uint32_t* fail_arr); //save failure array to failure storage
+uint8_t RAM_SaveFailArray(uint32_t* storage,uint32_t* fail_arr, uint32_t* storage_info); //save failure array to failure storage
 
 void RAM_StorageClear(uint32_t* storage); //clear failure storage
